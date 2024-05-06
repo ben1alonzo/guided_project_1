f2b68a39c134487d4d443fe35ab9fa147cb16658
Muhammad Jamal <jamal@test.com>
2024-05-06 12:39:22 -0400

    added get commands for film name

---
+++ film.js
@@ -0,0 +1,75 @@
+let nameH1;
+let filmsDiv;
+let planetDiv;
+const baseUrl = `https://swapi2.azurewebsites.net/api`;
+
+// Runs on page load
+addEventListener("DOMContentLoaded", () => {
+  nameH1 = document.querySelector("h1#name");
+
+  homeworldSpan = document.querySelector("span#homeworld");
+  planetUl = document.querySelector("#planets>ul");
+  characterUl = document.querySelector("#characters>ul");
+  const sp = new URLSearchParams(window.location.search);
+  const id = sp.get("id");
+  getFilm(id);
+  console.log(id.title);
+});
+
+async function getFilm(id) {
+  let film;
+  try {
+    film = await fetchFilm(id);
+    film.planets = await fetchPlanets(film);
+    film.characters = await fetchCharacters(film);
+    console.log(film.planets.name);
+  } catch (ex) {
+    console.error(`Error reading film ${id} data.`, ex.message);
+  }
+  renderFilm(film);
+}
+async function fetchFilm(id) {
+  let filmUrl = `${baseUrl}/films/${id}`;
+  return await fetch(filmUrl).then((res) => res.json());
+}
+
+async function fetchPlanets(film) {
+  const url = `${baseUrl}/films/${film?.id}/planets`;
+  const planet = await fetch(url).then((res) => res.json());
+  return planet;
+}
+
+async function fetchCharacters(film) {
+  const url = `${baseUrl}/films/${film?.id}/characters`;
+  const characters = await fetch(url).then((res) => res.json());
+  return characters;
+}
+
+const renderFilm = (film) => {
+  document.title = `SWAPI - ${film?.title}`; // Just to make the browser tab say their name
+  nameH1.textContent = film?.title;
+
+  const planetLis = film?.planets?.map(
+    (planets) =>
+      `<li><a href="/planets.html?id=${planets.id}">${planets.name}</li>`
+  );
+  planetUl.innerHTML = planetLis.join("");
+
+  const characterLis = film?.characters?.map(
+    (characters) =>
+      `<li><a href="/character.html?id=${characters.id}">${characters.name}</li>`
+  );
+  characterUl.innerHTML = characterLis.join("");
+};
+const renderCharacters = (characters) => {
+  const divs = characters.map((characters) => {
+    const el = document.createElement("div");
+    el.addEventListener("click", () => goToCharacterPage(film.id));
+    el.textContent = characters.name;
+    return el;
+  });
+  charactersList.replaceChildren(...divs);
+  console.log(characters);
+};
+
+const goToPlanetPage = (id) => (window.location = `/Planets.html?id=${id}`);
