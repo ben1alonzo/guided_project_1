6c5be02dacc493690ff6c802cd146dea0ada1a47
Muhammad Jamal <jamal@test.com>
2024-05-06 13:49:08 -0400

    updated films

--- film.js
+++ film.js
@@ -1,13 +1,21 @@
 let nameH1;
 let filmsDiv;
 let planetDiv;
+let producer;
+let director;
+let releaseDate;
+let episodeID;
+let openingCrawl;
 const baseUrl = `https://swapi2.azurewebsites.net/api`;
 
 // Runs on page load
 addEventListener("DOMContentLoaded", () => {
   nameH1 = document.querySelector("h1#name");
-
-  homeworldSpan = document.querySelector("span#homeworld");
+  producer = document.querySelector("span#producer");
+  director = document.querySelector("span#director");
+  releaseDate = document.querySelector("span#release_date");
+  episodeID = document.querySelector("span#episode_id");
+  openingCrawl = document.querySelector("span#opening_crawl");
   planetUl = document.querySelector("#planets>ul");
   characterUl = document.querySelector("#characters>ul");
   const sp = new URLSearchParams(window.location.search);
@@ -48,7 +56,11 @@ async function fetchCharacters(film) {
 const renderFilm = (film) => {
   document.title = `SWAPI - ${film?.title}`; // Just to make the browser tab say their name
   nameH1.textContent = film?.title;
-
+  producer.textContent = film?.producer;
+  director.textContent = film?.director;
+  releaseDate.textContent = film?.release_date;
+  episodeID.textContent = film?.episode_id;
+  openingCrawl.textContent = film?.opening_crawl;
   const planetLis = film?.planets?.map(
     (planets) =>
       `<li><a href="/planets.html?id=${planets.id}">${planets.name}</li>`
